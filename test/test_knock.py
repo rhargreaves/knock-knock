6610e6e0155ddf2bd68ea83a18307b7c5679858c
Robert Hargreaves <kiyouta@gmail.com>
2025-08-26 23:10:18 +0100

    refactor: remove old nc-based code

--- test/test_knock.py
+++ test/test_knock.py
@@ -1,36 +1,9 @@
 import socket
-import subprocess
 
 import pytest
 from conftest import wait_for_trace
 
 TARGET_PORT = 6666
-
-
-def port_filtered(dst_ip, port):
-    cmd = ["nc", "-zvw", "2", dst_ip, str(port)]
-    print(f"Running command: {' '.join(cmd)}")
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    print(f"Command output:\n{result.stdout}")
-    if result.stderr:
-        print(f"Command stderr:\n{result.stderr}")
-
-    if result.returncode == 1:
-        if "Connection refused" in result.stderr:
-            return False
-        elif "timed out" in result.stderr or result.stderr.strip() == "":
-            return True
-    return False
-
-
-def port_closed(dst_ip, port):
-    cmd = ["nc", "-z", dst_ip, str(port)]
-    print(f"Running command: {' '.join(cmd)}")
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    print(f"Command output:\n{result.stdout}")
-    if result.stderr:
-        print(f"Command stderr:\n{result.stderr}")
-    return result.returncode == 1
 
 
 @pytest.mark.usefixtures("loader")
@@ -61,7 +34,7 @@ def send_udp_packet_from_ip(dst, port, src_ip):
         sock.close()
 
 
-def port_closed_native(dst, port):
+def port_closed(dst, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1.0)
     try:
@@ -73,7 +46,7 @@ def port_closed_native(dst, port):
         sock.close()
 
 
-def port_filtered_native(dst, port):
+def port_filtered(dst, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1.0)
     try:
@@ -96,7 +69,7 @@ def test_port_closed_when_correct_code_udp_packet_sent():
     send_udp_packet(dst, CODE_1)
 
     # should be closed rather than filtered now
-    assert port_closed_native(dst, TARGET_PORT)
+    assert port_closed(dst, TARGET_PORT)
 
 
 @pytest.mark.usefixtures("loader")
@@ -107,7 +80,7 @@ def test_port_filtered_when_correct_code_udp_packet_sent_from_wrong_ip():
 
     send_udp_packet_from_ip(dst, CODE_1, "127.0.0.5")
 
-    assert port_filtered_native(dst, TARGET_PORT)
+    assert port_filtered(dst, TARGET_PORT)
 
 
 @pytest.mark.usefixtures("loader")
@@ -118,4 +91,4 @@ def test_port_filteed_when_wrong_code_udp_packet_sent():
 
     send_udp_packet(dst, WRONG_CODE_1)
 
-    assert port_filtered_native(dst, TARGET_PORT)
+    assert port_filtered(dst, TARGET_PORT)
