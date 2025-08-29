69d7e821a2a87bedb11fb9a2ed3194866c980ef5
Robert Hargreaves <kiyouta@gmail.com>
2025-08-30 00:19:19 +0100

    feat: more responsive exiting

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <vector>
 #include <optional>
 #include <system_error>
+#include <thread>
+#include <chrono>
 #include "knock.h"
 #include "bpf_program.hpp"
 #include "cli_args.hpp"
@@ -84,7 +86,7 @@ int main(int argc, char** argv)
         std::cout << "waiting for packets (Ctrl+C to exit)...\n";
 
         while (keep_running) {
-            sleep(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
 
         std::cout << "detached XDP program from " << args->interface << '\n';
